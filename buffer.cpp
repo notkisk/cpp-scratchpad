c3873f694b9e8c0ebbda06778ecb08faa36c3c29
notkisk <salahxd99@gmail.com>
2026-08-05 01:25:05 +0100

    something

---
+++ buffer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//
+int main(){
+int x, y ,z {};
+  std::cout << "enter three values x, y, z" << "\n";
+  std::cin >> x ;
+  std::cin>> y;
+  std::cin>> z;
+  std::cout <<"x = " << x<< " y = " << y << " z = " << z << "\n";
+  std::cin.get();
+  return 0;
+}
